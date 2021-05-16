ffaebdb62dba36aad910d3e4b3f84fc14b8af196
ithinkandicode <ithinkandicode@gmail.com>
2021-05-16 16:11:07 +0100

    Temp - Remove temp4

--- MODS/Working Directory 4 - GameDir/UPLOAD.bat
+++
@@ -1,8 +0,0 @@
-@echo Changing to game directory
-cd /d "D:\GAMES\Steam\steamapps\common\Door Kickers - Action Squad"
-
-@echo Initiating mod upload...
-ActionSquad.exe +upload_workshop mod_root
-
-:FINISHED_OK
-@echo Exiting...
